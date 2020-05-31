d35fe94e98d39338367ecbba72c9acc2ec812ec6
Kerem San <keremsan@Kerems-MacBook-Pro-2.local>
2020-05-31 17:49:59 +0300

    first commit

--- trade.py
+++ trade.py
@@ -3,7 +3,6 @@ import alpaca_trade_api as tradeapi
 api = tradeapi.REST()
 
 
-
 def checkMarket():
 	clock = api.get_clock()
 	return clock.is_open
@@ -13,11 +12,11 @@ def main():
 	if (not checkMarket()):
 		print("\nMarket is currently closed, exiting.\n")
 		exit()
-
+		
 	print("\nMarket is currently open\n")
 
 	while (checkMarket()):
-		print("Market Open")
+		print("\nMarket Open\n")
 		sleep(60)
 
 if __name__ == '__main__':
