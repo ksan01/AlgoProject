c15f5eda790963265d7d81788a177d06350785ba
Kerem San <keremsan@Kerems-MacBook-Pro-2.local>
2020-05-31 21:08:28 +0300

    added stock.py

---
+++ stock.py
@@ -0,0 +1,18 @@
+
+class Stock:
+
+	def __init__(self, name):
+		self.name   = name
+		self.price  = 0
+		self.avg    = 0
+		self.zscore = 0
+		self.count  = 0
+
+	def printPrice(self):
+		print("\n" + self.name + ": " + str(self.price) + "\n")
+
+	def printTrade(self, act):
+		print("\n" + act + " order for " + self.name + " at " + str(self.price))
+		print("\nPrice: " + str(self.price))
+		print("\n1-hour average: " + str(self.avg))
+		print("\nZ-Score: " + str(self.zscore) + "\n")
