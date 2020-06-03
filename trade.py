51dea5db5b942f8fc0d1b1aff38cd062ae6d4e07
Kerem San <keremsan@Kerems-MacBook-Pro-2.local>
2020-06-03 22:33:54 +0300

    started trade function

--- trade.py
+++ trade.py
@@ -10,6 +10,9 @@ API = tradeapi.REST()
 SYMBOL = 'AAPL'
 BUY  = 'BUY'
 SELL = 'SELL'
+BUY_FACTOR = -1.0
+SELL_FACTOR = 1.0
+
 
 def checkMarket():
 	clock = API.get_clock()
@@ -30,7 +33,8 @@ def getPrices(stock):
 	last_trade  = API.get_last_trade(SYMBOL)
 	stock.price = last_trade.price 
 
-	# get closing prices for each minute in the last hour
+	# get closing prices for each minute in the last hour 
+	# to compute 1-hour moving average of the stock
 	barset      = API.get_barset(SYMBOL, '1Min', limit=60)
 	bars        = barset[SYMBOL]
 	prices      = [bar.c for bar in bars]
@@ -40,6 +44,19 @@ def getPrices(stock):
 	std = statistics.stdev(prices)
 	stock.zscore = (stock.price - stock.avg) / std
 	print(stock.zscore)
+
+def trade(stock):
+
+	if (stock.zscore > SELL_FACTOR):
+		print(stock.zscore)
+		stock.printTrade(SELL)
+
+	elif (stock.zscore < BUY_FACTOR):
+		print(stock.zscore)
+		stock.printTrade(BUY)
+
+	else:
+		print("\nNo order for", stock.name, "\n")
 
 def main():
 	
@@ -53,9 +70,13 @@ def main():
 	while (checkMarket()):
 		getPrices(stock)
 		printStock(stock)
+		trade(stock)
 
 		print("\nRefreshing...\n")
+		# TODO: change to 60s 
 		sleep(10)
+
+
 
 if __name__ == '__main__':
 	main()
